81c617a00cf39e607306b0909322784448b4896f
arun7pulse <arun7pulse@gmail.com>
2018-05-01 16:30:35 +0530

    Create Get-ServiceStatus.ps1

---
+++ Get-ServiceStatus.ps1
@@ -0,0 +1,8 @@
+param(
+[string[]]$ComputerName = @("localhost"),
+[string[]]$ServiceName = @('notepad','mspaint')
+)
+
+Get-Service -Name $ServiceName -ComputerName $ComputerName | Select -Property MachineName,Name,Status,DisplayName | Sort MachineName,Name | Format-Table -AutoSize | Out-File -Force ./SMSServicDetails.txt
+
+./SMSServicDetails.txt
