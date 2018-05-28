f6c49745d8e699229dfc105eb013f2e23e97d318
ArunSanthoshKumar <arun7pulse@gmail.com>
2018-05-28 19:02:14 +0530

    Create Get-ServiceDependencies.ps1

---
+++ Get-ServiceDependencies.ps1
@@ -0,0 +1,16 @@
+
+param(
+[string[]]$ServiceName=""
+)
+Foreach($i in $Servicename ){
+Get-Service -ComputerName localhost | #Where-Object { $_.Name -eq $i} | 
+ForEach-Object { 
+    write-host -ForegroundColor 3 "ServiceName :: $($_.name)"  
+    if($_.DependentServices)  { write-host -ForegroundColor 5 "`tServices that depend on $($_.name)"  
+      foreach($s in $_.DependentServices){ "`t`t" + $s.name } 
+    } #end if DependentServices 
+    if($_.RequiredServices) { Write-host -ForegroundColor 10 "`tServices required by $($_.name)" 
+      foreach($r in $_.RequiredServices){ "`t`t" + $r.name } 
+    } #end if DependentServices 
+} 
+}#end foreach-object
