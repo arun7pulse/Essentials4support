7e7edb8bd05041523aa5978f7272799e0d20c9bb
ArunSanthoshKumar <arun7pulse@gmail.com>
2018-09-22 01:13:51 +0530

    Delete Get-PingInfo.ps1

--- Get-PingInfo.ps1
+++
@@ -1,9 +0,0 @@
-#REM Author - ArunSanthoshKumar. 
-
-param(
-[string[]]$ComputerName = @('localhost')
-)
-
-Test-Connection $ComputerName -count 1 |Format-Table -AutoSize | Out-File -Force ./PingDetails.txt
-
-./PingDetails.txt
